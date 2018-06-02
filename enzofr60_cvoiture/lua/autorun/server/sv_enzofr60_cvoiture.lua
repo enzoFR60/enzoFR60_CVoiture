69c265094faa858ccb58d10e7d5ab9da7cbc0f36
enzoFR60 <37971887+enzoFR60@users.noreply.github.com>
2018-06-02 06:56:43 +0200

    Update sv_enzofr60_cvoiture.lua
    Merci à Driven
    Thanks to Driver

--- enzofr60_cvoiture/lua/autorun/server/sv_enzofr60_cvoiture.lua
+++ enzofr60_cvoiture/lua/autorun/server/sv_enzofr60_cvoiture.lua
@@ -1,50 +1,39 @@
-include("autorun/sh_configcvoiture.lua")
+local version = "1.1" -- Don't touch ! Pas Touche !
+local ranksAdmins = { ["superadmin"] = true }
 
-local function CheckVersionCVoiture( ply )
-	for k, v in pairs(player.GetAll()) do
-	if v:GetUserGroup() == "superadmin" and enzoFR60.CVoiture.CheckVersionAddon then
-	timer.Create("InitialTimerVersionCheckCVoiture", 10, 1, function()
-		versionCVoiture()
-	end)
-	end
-	end
+function enzoFR60_CVoiture_Notify(ply, pType, pTime, pMsg)
+	if not IsValid(ply) then return end
+	if DarkRP then DarkRP.notify(ply, tonumber(pType), tonumber(pTime), string.Trim(pMsg))
+	else ply:PrintMessage(HUD_PRINTTALK, string.Trim(pMsg)) end
 end
-hook.Add( "PlayerInitialSpawn", "InitialVersionCheckCVoiture", CheckVersionCVoiture )
 
-VersionFrCVoiture = 1.0 -- Don't Touch ! Pas Touche !
+local function parsingVersion(pVersion)
+  return tonumber(string.Replace(tostring(pVersion), ".", ""))
+end
 
-local versionCheck = 0
-function versionCVoiture()
-    versionCheck = versionCheck+1;
-    local newVersion = nil
-	for k, v in pairs(player.GetAll()) do
-	if v:GetUserGroup() == "superadmin" and enzoFR60.CVoiture.CheckVersionAddon then
+local function checkVersion(ply)
+  if not IsValid(ply) then return end
+  if ranksAdmins[ply:GetUserGroup()] and enzoFR60.CVoiture.CheckVersionAddon then
     http.Fetch("https://raw.githubusercontent.com/enzoFR60/enzoFR60_CVoiture/master/version.txt",
-        function(body, len, headers, code)
-
-            newVersion = tonumber(body)
-
-            if newVersion > VersionFrCVoiture then
-                DarkRP.notify( v, 0, 20, "[CVoiture] "..enzoFR60.CVoiture.LanguageCheckVersionNonValide )
-			else
-				DarkRP.notify( v, 0, 20, "[CVoiture] "..enzoFR60.CVoiture.LanguageCheckVersionValide )
-		   end
-
-        end,
-        function(error)
-
-            if versionCheck != 1 then
-				DarkRP.notify( v, 0, 20, "[CVoiture] "..enzoFR60.CVoiture.LanguageCheckVersionError )
-			end
-
-            timer.Create("CVoitureReCheck", 40, 1, function()
-                CheckVersionCVoiture()
-            end)
-
+    	function(body, len, headers, code)
+      	if code == 404 then
+     	  	timer.Simple(300, function() if IsValid(ply) then checkVersion(ply) end end)
+       	 	enzoFR60_CVoiture_Notify(ply, 1, 20, "[CVoiture] " .. tostring(enzoFR60.CVoiture.LanguageCheckVersionError))
+          return
         end
-		
-    )
-	end
-	end
-
+        
+				if parsingVersion(string.Trim(body)) > parsingVersion(version) then
+        	enzoFR60_CVoiture_Notify(ply, 1, 20, "[CVoiture] " .. tostring(enzoFR60.CVoiture.LanguageCheckVersionNonValide))
+				end
+			end,
+      function(err)
+    	end)
+  end
 end
+
+hook.Add( "PlayerInitialSpawn", "InitialVersionCheckCVoiture", function(ply)
+  if not IsValid(ply) then return end
+	if ranksAdmins[ply:GetUserGroup()] and enzoFR60.CVoiture.CheckVersionAddon then
+		timer.Simple(5, function() if IsValid(ply) then checkVersion(ply) end end)
+	end
+end)
