714803fdbbaba9868279a30fad311316bf2b9d9f
enzoFR60 <37971887+enzoFR60@users.noreply.github.com>
2018-06-05 08:34:28 +0200

    Update sh_configcvoiture.lua

--- enzofr60_cvoiture/lua/autorun/sh_configcvoiture.lua
+++ enzofr60_cvoiture/lua/autorun/sh_configcvoiture.lua
@@ -1,4 +1,5 @@
 enzoFR60 = enzoFR60 or {}
+
 enzoFR60.CVoiture = enzoFR60.CVoiture or {}
 
 enzoFR60.CVoiture.CheckVersionAddon = true -- true = La verification marche, false = elle marche pas ;)
@@ -75,6 +76,8 @@ if (enzoFR60.CVoiture.SelectedLanguage == "en") then
  
  -- Notif Check Version
  
+ enzoFR60.CVoiture.LanguageCheckVersionValide = "You have the right version of the addon enzoFR60_cvoiture."
+ 
  enzoFR60.CVoiture.LanguageCheckVersionNonValide = "The version of the addon enzoFR60_cvoiture is not up to date."
  
  enzoFR60.CVoiture.LanguageCheckVersionError = "There was an error at the time of the audit"
@@ -128,6 +131,8 @@ elseif (enzoFR60.CVoiture.SelectedLanguage == "fr") then
  enzoFR60.CVoiture.LanguageArmePhone = "Telephone Du Restaurateur"
  
  -- Notif Check Version
+ 
+ enzoFR60.CVoiture.LanguageCheckVersionValide = "Vous avez la bonne version de l'addon enzoFR60_cvoiture."
  
  enzoFR60.CVoiture.LanguageCheckVersionNonValide = "La version de l'addon enzoFR60_cvoiture est pas a jour."
  
